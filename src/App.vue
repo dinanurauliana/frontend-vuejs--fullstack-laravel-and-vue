30099efc2b6f4630c02c3a1b7a046ec008a2e1eb
Dina Nur Auliana <dinakayla29@gmail.com>
2023-12-22 10:03:57 +0700

    Updated project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import TasksPages from './pages/TasksPage.vue'
+import Footer from './components/Footer.vue';
+
+</script>
+
+<template>
+  <!-- Navbar -->
+  <Navbar />
+
+  <!-- Content -->
+  <TasksPages />
+
+  <!-- Footer -->
+  <Footer />
+</template>
+
+<style scoped></style>
